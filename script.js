f1d8f6d240022ef5cf465c173276d101cee16881
nabe-ch <benatawa0608@icloud.com>
2026-06-27 22:07:18 +0900

    JavaScript準備

---
+++ script.js
@@ -0,0 +1,20 @@
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const zero = document.querySelector(".zero");
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const multi = document.querySelector(".multi");
+const division = document.querySelector(".division");
+const lisetButton = document.querySelector(".lisetButton");
+const equalButton = document.querySelector(".equalButton");
+const monitor = document.querySelector(".monitor")
+
+monitor.value = 5;
+console.log("モニターを変更");
