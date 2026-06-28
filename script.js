d971edc9fd3764576ef1020ccc66f5d1901d202a
nabe-ch <benatawa0608@icloud.com>
2026-06-28 22:48:03 +0900

    数字、リセット、バックボタン作動

--- script.js
+++ script.js
@@ -14,7 +14,46 @@ const multi = document.querySelector(".multi");
 const division = document.querySelector(".division");
 const lisetButton = document.querySelector(".lisetButton");
 const equalButton = document.querySelector(".equalButton");
-const monitor = document.querySelector(".monitor")
+const backButton = document.querySelector(".backButton");
+const monitor = document.querySelector(".monitor");
+let monitorNum;
 
-monitor.value = 5;
-console.log("モニターを変更");
+document.addEventListener(("click"),(event)=>{
+    const btn = event.target;
+
+    if(btn.tagName !== "BUTTON"){//これないとundefined入る
+        return;
+    };
+
+    if(btn === lisetButton){//モニターのリセット
+        monitor.value = 0;
+        console.log("リセットしました");
+
+    } else if(btn === equalButton){//計算用ボタン
+        console.log("計算しました");
+
+    } else if(btn === backButton){//後ろから１桁削除
+        monitor.value = monitor.value.slice(0,-1)
+        console.log("１桁削除");
+        if(monitor.value === ""){
+            monitor.value = 0;
+        };
+        //演算用ボタン
+    }else if(btn === plus){
+        console.log("プラス");
+    }else if(btn === minus){
+        console.log("マイナス");
+    }else if(btn === multi){
+        console.log("かける");
+    }else if(btn === division){
+        console.log("わる");    
+        
+    }else{//oneの部分を全ボタン対応にする →クリア
+        if(monitor.value == 0){//初期値が０なら消して数字を追加
+            monitor.value = btn.value;
+        }else{
+            monitor.value = monitor.value + btn.value;
+            console.log(`${btn.value}を追加`);
+        };
+    };
+});
